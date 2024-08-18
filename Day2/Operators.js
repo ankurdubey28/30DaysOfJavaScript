65f28f2be9d623b3b780de96b251b90cf7000be1
ankur dwivedi <ankurdwivedi.dce@gmail.com>
2024-08-18 13:06:01 +0530

    Day2 added

---
+++ Day2/Operators.js
@@ -0,0 +1,39 @@
+// Arithmetic Operators
+console.log(3+4)
+console.log(3-4)
+console.log(5/2)
+console.log(2*9)
+console.log(2**3)
+console.log(16%5)
+
+
+// Comparison Operators
+console.log(5<2)
+console.log(3>1)
+console.log(1>=1)
+console.log(null == undefined)
+console.log(null === undefined)
+
+
+// Assignment Operators
+let num=3;
+num++;
+num+=1
+num-=1
+num--;
+console.log(num)
+
+
+// Ternary Operator
+let check=num>=0?"positive":"negative";
+console.log(check)
+
+// Logical Operators
+let name="ankur"
+let age=33
+console.log(name==="ankur" && age===30)
+console.log(name==="ankur" || age===30)
+let bool=true
+console.log(!bool)
+
+
