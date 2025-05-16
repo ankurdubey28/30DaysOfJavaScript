3bb156f1405ed261824bc625c95af68afd1f121d
ankurdubey28 <118285729+ankurdubey28@users.noreply.github.com>
2025-05-16 13:48:58 +0530

    Update Loops.js

--- Day4/Loops.js
+++ Day4/Loops.js
@@ -33,4 +33,32 @@ function Pattern(num){
     }
 }
 
-//Pattern(5)
+// Destructuring:
+// 1.Binding vs assignment
+const[a=1]=[]; // default value
+const[a,,b]=[1,2,3] // ignoring values
+const[c:d]=[1] // renaming
+
+// variable swapping
+let a=1;
+let b=2;
+[a,b]=[b,a]
+console.log(a)
+console.log(b)
+
+
+// setting function parameter default value:
+// allows better handling of cases which can throw error
+function drawChart({
+  size = "big",
+  coords = { x: 0, y: 0 },
+  radius = 25,
+} = {}) {
+  console.log(size, coords, radius);
+  // do some chart drawing
+}
+
+drawChart({
+  coords: { x: 18, y: 30 },
+  radius: 30,
+});
